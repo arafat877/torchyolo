d52abcf086df0255c7a139c30abc20930c8c73b7
kadirnar <kadir.nar@hotmail.com>
2022-01-13 00:18:18 +0300

    detect kodu basitleştirildi ve run ederken output vermiyor

--- torchyolo/predict.py
+++ torchyolo/predict.py
@@ -1,128 +1,14 @@
-from __future__ import division
-import os, tqdm, random, torch
-import numpy as np
-from PIL import Image
-import torchvision.transforms as transforms
+import cv2
+import torch
 from torch.utils.data import DataLoader
-from torch.autograd import Variable
+from torchvision.transforms import transforms
 from torchyolo.models import load_model
-from torchyolo.utils.utils import load_classes, rescale_boxes, non_max_suppression
-from torchyolo.utils.datasets import ImageFolder
-from torchyolo.utils.transforms import Resize, DEFAULT_TRANSFORMS
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from matplotlib.ticker import NullLocator
+from torchyolo.utils.datasets import  ImageFolder
+from torchyolo.utils.transforms import DEFAULT_TRANSFORMS, Resize
+from torchyolo.utils.utils import non_max_suppression, load_classes
 
 
-def detect_directory():
-    dataloader = _create_data_loader(img_path, batch_size, img_size, n_cpu)
-    model = load_model(model_path, weights_path)
-    img_detections, imgs = detect(
-        model,
-        dataloader,
-        output_path,
-        conf_thres,
-        nms_thres)
-    _draw_and_save_output_images(
-        img_detections, imgs, img_size, output_path, classes)
-
-    print(f"---- Detections were saved to: '{output_path}' ----")
-
-
-def detect_image(model, image, img_size=416, conf_thres=0.5, nms_thres=0.5):
-    model.eval()  # Set model to evaluation mode
-    # Configure input
-    input_img = transforms.Compose([
-        DEFAULT_TRANSFORMS,
-        Resize(img_size)])(
-        (image, np.zeros((1, 5))))[0].unsqueeze(0)
-
-    if torch.cuda.is_available():
-        input_img = input_img.to("cuda")
-
-    # Get detections
-    with torch.no_grad():
-        detections = model(input_img)
-        detections = non_max_suppression(detections, conf_thres, nms_thres)
-        detections = rescale_boxes(detections[0], img_size, image.shape[:2])
-    return detections.numpy()
-
-
-def detect(model, dataloader, output_path, conf_thres, nms_thres):
-    # Create output directory, if missing
-    os.makedirs(output_path, exist_ok=True)
-    model.eval()  # Set model to evaluation mode
-    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
-    img_detections = []  # Stores detections for each image index
-    imgs = []  # Stores image paths
-
-    for (img_paths, input_imgs) in tqdm.tqdm(dataloader, desc="Detecting"):
-        # Configure input
-        input_imgs = Variable(input_imgs.type(Tensor))
-
-        # Get detections
-        with torch.no_grad():
-            detections = model(input_imgs)
-            detections = non_max_suppression(detections, conf_thres, nms_thres)
-
-        # Store image and detections
-        img_detections.extend(detections)
-        imgs.extend(img_paths)
-    return img_detections, imgs
-
-
-def _draw_and_save_output_images(img_detections, imgs, img_size, output_path, classes):
-    # Iterate through images and save plot of detections
-    for (image_path, detections) in zip(imgs, img_detections):
-        print(f"Image {image_path}:")
-        _draw_and_save_output_image(
-            image_path, detections, img_size, output_path, classes)
-
-
-def _draw_and_save_output_image(image_path, detections, img_size, output_path, classes):
-    # Create plot
-    img = np.array(Image.open(image_path))
-    plt.figure()
-    fig, ax = plt.subplots(1)
-    ax.imshow(img)
-    # Rescale boxes to original image
-    detections = rescale_boxes(detections, img_size, img.shape[:2])
-    unique_labels = detections[:, -1].cpu().unique()
-    n_cls_preds = len(unique_labels)
-    # Bounding-box colors
-    cmap = plt.get_cmap("tab20b")
-    colors = [cmap(i) for i in np.linspace(0, 1, n_cls_preds)]
-    bbox_colors = random.sample(colors, n_cls_preds)
-
-    for x1, y1, x2, y2, conf, cls_pred in detections:
-        print(f"\t+ Label: {classes[int(cls_pred)]} | Confidence: {conf.item():0.4f}")
-        box_w = x2 - x1
-        box_h = y2 - y1
-        color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
-        # Create a Rectangle patch
-        bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2, edgecolor=color, facecolor="none")
-        # Add the bbox to the plot
-        ax.add_patch(bbox)
-        # Add label
-        plt.text(
-            x1,
-            y1,
-            s=classes[int(cls_pred)],
-            color="white",
-            verticalalignment="top",
-            bbox={"color": color, "pad": 0})
-
-    # Save generated image with detections
-    plt.axis("off")
-    plt.gca().xaxis.set_major_locator(NullLocator())
-    plt.gca().yaxis.set_major_locator(NullLocator())
-    filename = os.path.basename(image_path).split(".")[0]
-    output_path = os.path.join(output_path, f"{filename}.png")
-    plt.savefig(output_path, bbox_inches="tight", pad_inches=0.0)
-    plt.close()
-
-
-def _create_data_loader(img_path, batch_size, img_size, n_cpu):
+def create_data_loader():
     dataset = ImageFolder(
         img_path,
         transform=transforms.Compose([DEFAULT_TRANSFORMS, Resize(img_size)]))
@@ -135,11 +21,32 @@ def _create_data_loader(img_path, batch_size, img_size, n_cpu):
     return dataloader
 
 
+def run():
+    # device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # data loader
+    dataloader = create_data_loader()
+    # model
+    model = load_model(model_path, weights_path)
+    model.to(device).eval()
+    # predict
+    for i, (img, _) in enumerate(dataloader):
+        with torch.no_grad():
+            output = model(img.to(device))
+            output = non_max_suppression(output, conf_thres)
+        print(output)
+        img = img.to('cpu').numpy().transpose(1, 2, 0)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        img = cv2.resize(img, (416, 416))
+        cv2.imwrite("deneme.jpg", img)
+        print(f"---- Detections were saved to: '{output_path}' ----")
+
+
 if __name__ == "__main__":
     model_path = "../cfg/yolov3-tiny.cfg"
     weights_path = "../weights/yolov3-tiny.weights"
-    img_path = "../tests/data"
+    img_path = "../tests/data/dog.jpg"
     output_path = "../output"
-    batch_size, img_size, n_cpu, conf_thres, nms_thres = 8, 416, 8, 0.5, 0.5
+    batch_size, img_size, n_cpu, conf_thres, nms_thres = 1, 416, 8, 0.5, 0.5
     classes = load_classes("../cfg/coco.names")
-    detect_directory()
+    run()
